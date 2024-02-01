ae997ff2a093d3ada69e7c70f13dcc7c3129ecfd
Suraj Gupta <sg198374@gmail.com>
2024-02-01 22:15:28 +0530

    DONE

--- AutomationProject_Framework/src/test/java/testcases/TestCase1.java
+++ AutomationProject_Framework/src/test/java/testcases/TestCase1.java
@@ -1,16 +1,46 @@
 package testcases;
 
 import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 public class TestCase1 {
-	@Test
-	public void SignUp() {
-		System.out.println("Sign Up into the portal");
+	
+	@BeforeTest    //It will run once before the BeforeMethod and all the methods(@Test)
+	public void Open() {
+		System.out.println("Click on the URL to access flipkart\n");
 	}
 	
-	@Test
-	public void SignOut() {
-		System.out.println("Sign Out from the portal");
+	@AfterTest   //It will run once after the AfterMethod and all the methods(@Test)
+	public void Close() {
+		System.out.println("Close the flipkart application\n");
+	}
+	
+	@BeforeMethod  //It will run before all the methods(@Test) start
+	public void Login() {
+		System.out.println("Login into the flipkart portal");
+	}
+	
+	@Test(priority=1)
+	public void Search() {
+		
+		System.out.println("Search for a smartphone");
+		System.out.println("Not like it, so will log-out");
+		AssertJUnit.assertEquals(true, true);
+	}
+	
+	@Test(priority=2, dependsOnMethods="Search")
+	public void Buy() {
+		System.out.println("Search for a watch");
+		System.out.println("Purchase a watch");
+		System.out.println("Payment done and order successful");
+	}
+	
+	@AfterMethod     //It will run after all the methods(@Test) start
+	public void LogOut() {
+		System.out.println("LogOut from the flipkart application\n");
 	}
 	
 }
