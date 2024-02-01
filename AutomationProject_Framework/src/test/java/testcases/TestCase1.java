9be4d1fae05f9c9e79137298250f26fac1f04178
Suraj Gupta <sg198374@gmail.com>
2024-02-01 21:27:25 +0530

    DONE

---
+++ AutomationProject_Framework/src/test/java/testcases/TestCase1.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	@Test
+	public void SignUp() {
+		System.out.println("Sign Up into the portal");
+	}
+	
+	@Test
+	public void SignOut() {
+		System.out.println("Sign Out from the portal");
+	}
+	
+}
