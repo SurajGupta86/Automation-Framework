98e457ce8028dd34d86f2a12694539e9e026db8d
Suraj Gupta <sg198374@gmail.com>
2024-02-06 11:01:49 +0530

    DONE

---
+++ AutomationProject_Framework/src/test/java/listener/Listeners.java
@@ -0,0 +1,18 @@
+package listener;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener{
+	
+	//Listener- To perform some actions automatically such as Test passed, test failed and screenshot should be captured.
+	//ITestListener- A kind of Interface to access different methods.
+	
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("Test Case Passed");
+	}
+	
+	public void onTestFailure(ITestResult result) {
+		System.out.println("Test Case Failed");
+	}
+}
